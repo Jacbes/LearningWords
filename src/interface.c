ca527c388735f86cec6fe3a46c37287cb417211a
ChtobiUchitsyaNadoBitProfi <ivan.mielnikov.01@mail.ru>
2019-04-01 16:25:15 +0700

    Create function Welcome_window

--- src/interface.c
+++ src/interface.c
@@ -1,1 +1,12 @@
 #include <stdio.h>
+
+void Welcome_window() {
+  printf("\033[2J"); /* Clear the entire screen. */
+  printf("\033[0;0f");
+  printf("\n");
+  printf("                         Welcome to the LearningWords \n");
+  printf("\n");
+  printf("                                    1) start \n");
+  printf("                                    2) info \n");
+  printf("                                    3) exit \n");
+}
