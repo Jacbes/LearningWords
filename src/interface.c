c7f2791b9f5b8f8d1e07b4cc90e97418f5fb8568
ChtobiUchitsyaNadoBitProfi <ivan.mielnikov.01@mail.ru>
2019-04-01 23:50:19 +0700

    Update function Welcome_window

--- src/interface.c
+++ src/interface.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 void Welcome_window() {
-  printf("\033[2J"); /* Clear the entire screen. */
+  printf("\033[2J");
   printf("\033[0;0f");
-  printf("\n");
-  printf("                         Welcome to the LearningWords \n");
-  printf("\n");
-  printf("                                    1) start \n");
-  printf("                                    2) info \n");
-  printf("                                    3) exit \n");
+  File_input("welcome.txt");
+  while (1) {
+    Input();
+    break;
+  }
 }
