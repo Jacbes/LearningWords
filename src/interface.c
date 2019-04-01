32eb56250aa70d03ccdda4181153c5431e4ffbcf
ChtobiUchitsyaNadoBitProfi <ivan.mielnikov.01@mail.ru>
2019-04-02 00:14:41 +0700

    Include inteface.h

--- src/interface.c
+++ src/interface.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "interface.h"
+
 
 void Welcome_window() {
   printf("\033[2J");
---
+++ src/interface.h
@@ -0,0 +1,19 @@
+#ifndef H_WELCOME
+#define H_WELCOME
+void Welcome_window();
+#endif
+
+#ifndef H_INPUT
+#define H_INPUT
+void Input();
+#endif
+
+#ifndef H_INFO
+#define H_INFO
+int Info();
+#endif
+
+#ifndef H_FILE_INPUT
+#define H_FILE_INPUT
+void File_input(char *fname);
+#endif
