714afd778530f30e25c9dfb08792cd2a1a288f12
ChtobiUchitsyaNadoBitProfi <ivan.mielnikov.01@mail.ru>
2019-04-01 23:54:16 +0700

    Create the function of file input

--- src/interface.c
+++ src/interface.c
@@ -9,3 +9,26 @@ void Welcome_window() {
     break;
   }
 }
+
+void Input() {
+  int l = 0;
+  while ((l != 1) && (l != 2) && (l != 3)) {
+    scanf("%d", &l);
+    switch (l) {
+    case 1:
+      printf("Soon...\n");
+      getchar();
+      Welcome_window();
+      break;
+    case 2:
+      Info();
+      break;
+    case 3:
+      exit(0);
+      break;
+    default:
+      printf("Error, try gain : ");
+      Welcome_window();
+    }
+  }
+}
