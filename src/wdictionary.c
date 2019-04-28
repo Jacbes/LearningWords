95d3e62f404c6435d25c40d126f4b8c317f259cb
ChtobiUchitsyaNadoBitProfi <ivan.mielnikov.01@mail.ru>
2019-04-28 23:48:01 +0700

    Update sourses

--- src/wdictionary.c
+++ src/wdictionary.c
@@ -1,161 +1,166 @@
 #include "wdictionary.h"
+#include "interface.h"
 #include "string.h"
 
-int getrand(int min, int max)
-{
-    return (double)rand() / (RAND_MAX + 1.0) * (max - min) + min;
+int getrand(int min, int max) {
+  return (double)rand() / (RAND_MAX + 1.0) * (max - min) + min;
 }
 
-int words_count(FILE* input, int value)
-{
-    int words = 0;
+int words_count(FILE *input, int value) {
+  int words = 0;
 
-    switch (value) {
-    case 1: {
-        while (!feof(input)) {
-            if (fgetc(input) == '\n') {
-                words++;
-            }
-        }
-        break;
+  switch (value) {
+  case 1: {
+    while (!feof(input)) {
+      if (fgetc(input) == '\n') {
+        words++;
+      }
     }
-    case 2: {
-        while (!feof(input)) {
-            if (fgetc(input) == '2') {
-                words++;
-            }
-        }
-        break;
+    break;
+  }
+  case 2: {
+    while (!feof(input)) {
+      if (fgetc(input) == '2') {
+        words++;
+      }
     }
-    case 3: {
-        while (!feof(input)) {
-            if (fgetc(input) == '3') {
-                words++;
-            }
-        }
-        break;
+    break;
+  }
+  case 3: {
+    while (!feof(input)) {
+      if (fgetc(input) == '3') {
+        words++;
+      }
     }
-    case 4: {
-        while (!feof(input)) {
-            if (fgetc(input) == '4') {
-                words++;
-            }
-        }
-        break;
+    break;
+  }
+  case 4: {
+    while (!feof(input)) {
+      if (fgetc(input) == '4') {
+        words++;
+      }
     }
-    }
+    break;
+  }
+  }
 
-    return words;
+  return words;
 }
 
-dictionary* dictionary_init(int words)
-{
-    int i, j;
-    if (words > 0) {
-        dictionary* tabinit = (dictionary*)malloc(words * sizeof(dictionary));
-        for (i = 0; i < words; i++) {
-            tabinit[i].engword = (char*)malloc(30 * sizeof(char));
-            tabinit[i].rusword = (char*)malloc(30 * sizeof(char));
-            tabinit[i].theme = (char*)malloc(sizeof(char));
-            for (j = 0; j < 30; j++) {
-                tabinit[i].engword[j] = 0;
-                tabinit[i].rusword[j] = 0;
-            }
-            tabinit[i].theme[0] = 0;
-        }
-        if (tabinit == NULL) {
-            return NULL;
-        } else {
-            return tabinit;
-        }
-    }
-
-    return NULL;
-}
-
-dictionary* dictionary_read(dictionary* tabinit, int words, FILE* input)
-{
-    rewind(input);
-
-    char* buf = (char*)malloc(30 * sizeof(char));
-    char* w[10];
-    int i;
-
+dictionary *dictionary_init(int words) {
+  int i, j;
+  if (words > 0) {
+    dictionary *tabinit = (dictionary *)malloc(words * sizeof(dictionary));
     for (i = 0; i < words; i++) {
-        fscanf(input, "%s", buf);
-        s_tok(buf, ';', w);
-        scopy(w[0], tabinit[i].engword);
-        scopy(w[1], tabinit[i].rusword);
-        scopy(w[2], tabinit[i].theme);
+      tabinit[i].engword = (char *)malloc(30 * sizeof(char));
+      tabinit[i].rusword = (char *)malloc(30 * sizeof(char));
+      tabinit[i].theme = (char *)malloc(sizeof(char));
+      for (j = 0; j < 30; j++) {
+        tabinit[i].engword[j] = 0;
+        tabinit[i].rusword[j] = 0;
+      }
+      tabinit[i].theme[0] = 0;
     }
+    if (tabinit == NULL) {
+      return NULL;
+    } else {
+      return tabinit;
+    }
+  }
 
-    free(buf);
-
-    return tabinit;
+  return NULL;
 }
 
-void dictionary_shuf(dictionary* tab, int words)
-{
-    int i, j;
+dictionary *dictionary_read(dictionary *tabinit, int words, FILE *input) {
+  rewind(input);
 
-    dictionary* buf = dictionary_init(1);
+  char *buf = (char *)malloc(30 * sizeof(char));
+  char *w[10];
+  int i;
 
-    for (i = 0; i < words; i++) {
-        j = getrand(0, i);
-        buf[0] = tab[i];
-        tab[i] = tab[j];
-        tab[j] = buf[0];
-    }
+  for (i = 0; i < words; i++) {
+    fscanf(input, "%s", buf);
+    s_tok(buf, ';', w);
+    scopy(w[0], tabinit[i].engword);
+    scopy(w[1], tabinit[i].rusword);
+    scopy(w[2], tabinit[i].theme);
+  }
 
-    free(buf);
+  free(buf);
+
+  return tabinit;
 }
 
-void answers(dictionary* tab, int words)
-{
-    srand(time(NULL));
-    dictionary_shuf(tab, words);
-    dictionary* var = dictionary_init(4);
-    int corr, num, numc, ans, corr_ans = 0;
-    int i, j, k;
+void dictionary_shuf(dictionary *tab, int words) {
+  int i, j;
 
-    for (k = 0; k < words; k++) {
-        corr = getrand(0, words - 1);
-        printf("Word is: %s\n\n", tab[corr].engword);
+  dictionary *buf = dictionary_init(1);
 
-        numc = getrand(0, 3);
-        var[numc].rusword = tab[corr].rusword;
+  for (i = 0; i < words; i++) {
+    j = getrand(0, i);
+    buf[0] = tab[i];
+    tab[i] = tab[j];
+    tab[j] = buf[0];
+  }
 
-        for (i = 0; i < 4;) {
-            num = getrand(0, words);
-            if (i != numc) {
-                while (num == corr) {
-                    num = getrand(0, words);
-                }
-                if (num != corr) {
-                    var[i].rusword = tab[num].rusword;
-                    i++;
-                } else {
-                    i++;
-                }
-            } else {
-                i++;
-            }
+  free(buf);
+}
+
+void answers(dictionary *tab, int words) {
+  srand(time(NULL));
+  dictionary_shuf(tab, words);
+  dictionary *var = dictionary_init(4);
+  int corr, num, numc, ans, corr_ans = 0;
+  int i, k, j;
+  printf("\033[2J");
+  printf("\033[0;0f");
+
+  for (k = 0; k < words; k++) {
+
+    corr = getrand(0, words - 1);
+    printf("%d/%d\n", k + 1, words);
+    printf("\n");
+    printf("                                      %s\n\n", tab[corr].engword);
+    printf("\n");
+    printf("\n");
+
+    numc = getrand(0, 3);
+    var[numc].rusword = tab[corr].rusword;
+
+    for (i = 0; i < 4;) {
+      num = getrand(0, words);
+      if (i != numc) {
+        while (num == corr) {
+          num = getrand(0, words);
         }
-
-        for (j = 0; j < 4; j++) {
-            printf("%d) %s\t", j + 1, var[j].rusword);
-        }
-
-        printf("\nEnter answer (1, 2, 3, 4): ");
-        scanf("%d", &ans);
-
-        if (tab[corr].rusword == var[ans - 1].rusword) {
-            printf("++Correct!\n");
-            corr_ans++;
+        if (num != corr) {
+          var[i].rusword = tab[num].rusword;
+          i++;
         } else {
-            printf("--Wrong\n");
+          i++;
         }
+      } else {
+        i++;
+      }
     }
+    for (j = 0; j < 4; j++) {
+      printf("\n                                  %d) %s\t  \n \n", j + 1,
+             var[j].rusword);
+    }
+    for (j = 0; j < 3; j++) {
+      printf("\n");
+    }
+    printf("\nEnter answer (1, 2, 3, 4): ");
+    scanf("%d", &ans);
 
-    printf("\nAll - %d\nCorrect - %d\n", words, corr_ans);
+    if (tab[corr].rusword == var[ans - 1].rusword) {
+      printf("++Correct!\n");
+      corr_ans++;
+    } else {
+      printf("--Wrong\n");
+    }
+    printf("\033[2J");
+    printf("\033[0;0f");
+  }
+  Result(corr_ans, words);
 }
